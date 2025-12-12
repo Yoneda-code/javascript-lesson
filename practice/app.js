ed6d571e5c1c2c2c0488ba7f649efb24177faf3e
Yoneda-code <akari@YonedaAkarinoMacBook-Pro.local>
2025-12-12 18:47:39 +0900

    first commit

---
+++ practice/app.js
@@ -0,0 +1,33 @@
+let text = 'JavaScriptの練習';
+console.log(text);
+text = 'JavaScriptをマスターした';
+console.log(text);
+// 処理①
+let longText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+console.log(longText);
+console.log(longText);
+console.log(longText);
+
+// 処理②
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(23);
+let age = 23;
+console.log(age);
+
+let num1 = 25;
+let num2 = 2;
+
+console.log(num1 + num2);
+let stringNum = '25';
+let numberNum = 2;
+
+console.log(stringNum + numberNum); // => 252
